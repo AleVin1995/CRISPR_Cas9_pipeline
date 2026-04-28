4df4420526b0827ed1f7887ece5be79992255da8
zxBIB Vinceti,Alessandro (CI CRM) EXTERNAL <alessandro.vinceti.ext@boehringer-ingelheim.com>
2026-04-28 17:02:56 +0300

    fixed suffix

--- src/compute_norm_lfc.R
+++ src/compute_norm_lfc.R
@@ -30,7 +30,7 @@ cell_line_basename <- basename(cell_line) %>%
     str_split_fixed("\\.", 2) %>%
     .[, 1]
 
-output_file <- paste0(cell_line_basename, "_raw_lfc.tsv")
+output_file <- paste0(cell_line_basename, "_norm_lfc.tsv")
 
 write.table(sorted_genome, file = output_file, sep = "\t", row.names = FALSE, quote = FALSE)
 write.table(norm_and_lfc$norm_counts, file = paste0(cell_line_basename, "_norm_counts.tsv"), 
