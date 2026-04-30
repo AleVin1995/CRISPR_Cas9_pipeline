f05e5d8893cf245aa1954b5faabd7ef60efcf4e4
zxBIB Vinceti,Alessandro (CI CRM) EXTERNAL <alessandro.vinceti.ext@boehringer-ingelheim.com>
2026-04-30 10:29:57 +0300

    added new parameters

--- modules/templates/compute_norm_lfc.R
+++ modules/templates/compute_norm_lfc.R
@@ -5,18 +5,22 @@ library(tidyverse)
 
 # Arguments
 opt <- list(
-    cell_line      = "${input_file}",
-    crispr_lib     = "${params.crispr_lib}",
-    min_reads      = as.numeric("${params.min_reads}"),
-    n_controls     = as.numeric("${params.n_controls}"),
-    scaling_method = "${params.scaling_method}"
+    cell_line            = "${input_file}",
+    crispr_lib           = "${params.crispr_lib}",
+    min_reads            = as.numeric("${params.min_reads}"),
+    n_controls           = as.numeric("${params.n_controls}"),
+    scaling_method       = "${params.scaling_method}",
+    min_treatments       = as.numeric("${params.min_treatments}"),
+    default_n_cols_sgrna = as.numeric("${params.default_n_cols_sgrna}")
 )
 
-cell_line        <- opt[['cell_line']]
-crispr_lib       <- opt[['crispr_lib']]
-min_reads        <- opt[['min_reads']]
-n_controls       <- opt[['n_controls']]
-scaling_method   <- opt[['scaling_method']]
+cell_line            <- opt[['cell_line']]
+crispr_lib           <- opt[['crispr_lib']]
+min_reads            <- opt[['min_reads']]
+n_controls           <- opt[['n_controls']]
+scaling_method       <- opt[['scaling_method']]
+min_treatments       <- opt[['min_treatments']]
+default_n_cols_sgrna <- opt[['default_n_cols_sgrna']]
 
 cell_line_basename <- sub("[.].+", "", basename(cell_line))
 
@@ -31,10 +35,10 @@ norm_and_lfc <- ccr.NormfoldChanges(cell_line,
                                     ncontrols = n_controls,
                                     method = scaling_method)
 
-# If number of treatment samples is < 2, abort the workflow
-n_treatment_samples <- ncol(norm_and_lfc[['norm_counts']]) - n_controls - 2
+# If number of treatment samples is < min_treatments, abort the workflow
+n_treatment_samples <- ncol(norm_and_lfc[['norm_counts']]) - n_controls - default_n_cols_sgrna
 
-if (n_treatment_samples < 2) {
+if (n_treatment_samples < min_treatments) {
     warning(paste0("At least two treatment samples are required to compute log fold changes for ",
         cell_line_basename, ". Skipping."))
 } else {
