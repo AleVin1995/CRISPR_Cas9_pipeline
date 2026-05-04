0345cd9ebf92d245627b28e77845111411378be7
zxBIB Vinceti,Alessandro (CI CRM) EXTERNAL <alessandro.vinceti.ext@boehringer-ingelheim.com>
2026-05-04 20:33:29 +0300

    Download sgrna counts if directory doesn't exist

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 IMAGE_NAME="crispr_cas9_pipeline:v1.0"
 TAR_FILE="crispr_pipeline_v1.tar.gz"
+ARTICLE_ID="32159628"
+
+# Check if data directory exists
+# If it doesn't exist, download the data and prepare it for the pipeline
+if [ ! -d "data/raw_sgrnas_counts" ]; then
+    echo "Raw sgrna counts directory not found. Downloading and preparing data..."
+
+    curl -s "https://api.figshare.com/v2/articles/$ARTICLE_ID" | \
+    jq -r '.files[].download_url' | \
+    xargs -n 1 curl -LJO
+
+    tar -xzvf raw_sgrnas_counts.tar.gz
+fi
 
 # Check if image exists locally
 if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
