cf5a7c5f8b2524cfa2f0f4883342c4900e6ca8e6
AleVin <alessandro.vinceti1995@gmail.com>
2026-05-07 20:42:22 +0300

    adjusted run pipeline

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -5,14 +5,18 @@ ARTICLE_ID="32159628"
 
 # Check if data directory exists
 # If it doesn't exist, download the data and prepare it for the pipeline
-if [ ! -d "data/raw_sgrnas_counts" ]; then
+if [ ! -d "data/raw_sgrnas_counts" ] || [ ! -d "tests/data" ]; then
     echo "Raw sgrna counts directory not found. Downloading and preparing data..."
 
     curl -s "https://api.figshare.com/v2/articles/$ARTICLE_ID" | \
     jq -r '.files[].download_url' | \
     xargs -n 1 curl -LJO
 
-    tar -xzvf raw_sgrnas_counts.tar.gz
+    tar -xzvf raw_sgrnas_counts.tar.gz && mv raw_sgrnas_counts data/
+    tar -xzvf test_data.tar.gz && mv test_data tests/ && \
+        mv tests/test_data tests/data
+
+    rm raw_sgrnas_counts.tar.gz test_data.tar.gz
 fi
 
 # Check if image exists locally
